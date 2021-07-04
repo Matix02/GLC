f87983bd5e95d96b984cd6cb7d6f066f47108ff2
mateusz <mateuszgeslowski3108@gmail.com>
2021-07-04 08:56:00 +0200

    Added NavigationComponent with BottomNavBar

--- app/src/main/java/com/example/glc/MainActivity.kt
+++ app/src/main/java/com/example/glc/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.glc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
+//Zastanowić się jak dodać to LoginActivity, by jako pierwsze było jeśli użytkownik nie jest zalogowany - zakładaka z StackOverFlow
+//Następnie View i Data Binding dodać
+//Następnie LogowanieGoogle z MVVM itd.
